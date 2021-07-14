7f6208dd6e3d618383717e844f8dc04badf54da4
cob16 <public+github@cormacbrady.info>
2021-07-14 14:25:30 +0100

    manage state in bootstrap module

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,5 +1,22 @@
 terraform {
   required_version = ">=1.0.2"
+
+  backend "s3" {
+    bucket = "\n-------------------------\nPlease read the README.md first 📖\n------------------------"
+    key    = "\n-------------------------\nPlease read the README.md first 📖\n------------------------"
+    region = "\n-------------------------\nPlease read the README.md first 📖\n------------------------"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.49"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1"
+    }
+  }
 }
 
 provider "aws" {
@@ -10,4 +27,27 @@ variable "boostrap_name" {
   description = "name that resources will be given (usually the name of the aws account)"
   type        = string
   default     = "platsec-ci"
+}
+
+locals {
+  backend_content = <<-EOT
+    bucket = "${aws_s3_bucket.s3_bucket.bucket}"
+    key = "bootstrap/v1"
+    region = "eu-west-2"
+  EOT
+}
+
+resource "aws_secretsmanager_secret" "backend" {
+  name       = "backend.hcl"
+  kms_key_id = aws_kms_key.terraform_key.key_id
+}
+
+resource "aws_secretsmanager_secret_version" "backend" {
+  secret_id     = aws_secretsmanager_secret.backend.id
+  secret_string = local.backend_content
+}
+
+resource "local_file" "backend" {
+  filename = "backend.hcl"
+  content  = local.backend_content
 }
