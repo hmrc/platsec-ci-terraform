c6666a6e00e7258e0caaf161badc4e3507f86b98
Vlastimil Zeman <vlastimil@zeman.dev>
2021-08-26 14:00:30 +0100

    PSEC-1269 Restrict deployment step access
    to endpoints only. No public internet.
    
    Co-authored-by: Cormac Brady <cob16@users.noreply.github.com>

--- modules/lambda_deployment_step/variables.tf
+++ modules/lambda_deployment_step/variables.tf
@@ -2,6 +2,18 @@ variable "name_prefix" {
   type = string
 }
 
+variable "vpc_config" {
+  type = object({
+    private_subnet_ids  = list(string),
+    private_subnet_arns = list(string),
+    vpc_id              = string,
+  })
+}
+
+variable "agent_security_group_ids" {
+  type = list(string)
+}
+
 variable "build_core_policy_arn" {
   type = string
   validation {
@@ -25,5 +37,3 @@ variable "lambda_arn" {
     error_message = "Arn must be given and should start with 'arn:aws:lambda:'."
   }
 }
-
-
--- modules/lambda_zip_pipeline/build.tf
+++ modules/lambda_zip_pipeline/build.tf
@@ -1,9 +1,11 @@
 module "build_artifact_step" {
-  source                   = "../build_artifact_step"
-  docker_required          = var.docker_build_required
-  build_core_policy_arn    = aws_iam_policy.build_core.arn
-  name_prefix              = local.full_name
-  s3_bucket_arn            = aws_s3_bucket.codepipeline_bucket.arn
+  source = "../build_artifact_step"
+
+  docker_required       = var.docker_build_required
+  build_core_policy_arn = aws_iam_policy.build_core.arn
+  name_prefix           = local.full_name
+  s3_bucket_arn         = aws_s3_bucket.codepipeline_bucket.arn
+
   vpc_config               = var.vpc_config
   agent_security_group_ids = [var.ci_agent_to_endpoints_sg_id, var.ci_agent_to_internet_sg_id]
 }
@@ -15,6 +17,9 @@ module "lambda-deployment-step-development" {
   name_prefix           = "${local.full_name}-development"
   build_core_policy_arn = aws_iam_policy.build_core.arn
   deployment_role_arn   = var.accounts.development.deployment_role_arn
+
+  vpc_config               = var.vpc_config
+  agent_security_group_ids = [var.ci_agent_to_endpoints_sg_id]
 }
 
 module "lambda-deployment-step-production" {
@@ -24,4 +29,7 @@ module "lambda-deployment-step-production" {
   name_prefix           = "${local.full_name}-production"
   build_core_policy_arn = aws_iam_policy.build_core.arn
   deployment_role_arn   = var.accounts.production.deployment_role_arn
+
+  vpc_config               = var.vpc_config
+  agent_security_group_ids = [var.ci_agent_to_endpoints_sg_id]
 }
