90b9030c76b340198d8a8f0a853b0b3e41738580
Anthony Robinson <anthony.robinson@madetech.com>
2021-12-16 16:09:31 +0000

    PSEC-1408 add GitHub git tagging step using the build-api

---
+++ modules/git_tag_step/outputs.tf
@@ -0,0 +1,7 @@
+output "name" {
+  value = aws_codebuild_project.git_tag.name
+}
+
+output "arn" {
+  value = aws_codebuild_project.git_tag.arn
+}
--- modules/terraform_module_pipeline/build.tf
+++ modules/terraform_module_pipeline/build.tf
@@ -24,3 +24,14 @@ module "git_version_step" {
   agent_security_group_ids = [var.ci_agent_to_endpoints_sg_id, var.ci_agent_to_internet_sg_id]
   policy_arns              = [module.common.policy_build_core_arn, module.common.policy_get_artifactory_credentials_arn]
 }
+
+module "git_tag_step" {
+  source = "../git_tag_step"
+
+  step_name       = "${module.common.pipeline_name}-git-tag"
+  repository_name = var.src_repo
+
+  vpc_config               = var.vpc_config
+  agent_security_group_ids = [var.ci_agent_to_endpoints_sg_id, var.ci_agent_to_internet_sg_id]
+  policy_arns              = [module.common.policy_build_core_arn, module.common.policy_get_artifactory_credentials_arn]
+}
