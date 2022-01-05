27ec3e74e8ce026759060e2964fdd0ec030df372
ulaskan <15844743+ulaskan@users.noreply.github.com>
2022-01-05 16:09:03 +0000

    working sns

--- sns.tf
+++ sns.tf
@@ -1,21 +1,12 @@
 resource "aws_sns_topic" "platsec_compliance_alerting_failure_topic" {
   name         = "platsec_compliance_alerting_failure_topic"
   display_name = "platsec_compliance_alerting_failure_topic"
-  policy       = ""
 }
 
 resource "aws_sns_topic_policy" "platsec_compliance_alerting_failure_topic_policy" {
   arn    = aws_sns_topic.platsec_compliance_alerting_failure_topic.arn
   policy = data.aws_iam_policy_document.platsec_compliance_alerting_failure_topic_policy_doc.json
 }
-
-#resource "aws_lambda_permission" "pipeline_notification" {
-#  statement_id  = "AllowExecutionFromSNS"
-#  action        = "lambda:InvokeFunction"
-#  function_name = "platsec_compliance_alerting_lambda"
-#  principal     = "sns.amazonaws.com"
-#  source_arn    = aws_sns_topic.platsec_compliance_alerting_failure_topic.arn
-#}
 
 data "aws_iam_policy_document" "platsec_compliance_alerting_failure_topic_policy_doc" {
   statement {
@@ -75,8 +66,4 @@ data "aws_iam_policy_document" "platsec_compliance_alerting_failure_topic_policy
     resources = [ aws_sns_topic.platsec_compliance_alerting_failure_topic.arn ]
   }
 
-}
-
-output "sns_arn" {
-  value = aws_sns_topic.platsec_compliance_alerting_failure_topic.arn
 }
