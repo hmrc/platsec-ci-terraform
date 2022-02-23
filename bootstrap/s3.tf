1a1bffa17ec1b4bcc690d3d9df14b3c817ca98ac
Marti Butler <5116631+martibutler@users.noreply.github.com>
2022-02-23 11:32:11 +0000

    PSEC-1472 Converting cf-templates bucket into TF and using new standard module

--- bootstrap/s3.tf
+++ bootstrap/s3.tf
@@ -11,7 +11,12 @@ data "aws_secretsmanager_secret_version" "s3_state_bucket_name" {
 }
 
 locals {
-  tf_provisioner_role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RoleTerraformProvisioner"
+  tf_provisioner_role    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RoleTerraformProvisioner"
+  security_eng_role      = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RoleSecurityEngineer"
+  changeset_creator_role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RoleChangeSetCreator"
+  iam_admin_role         = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RoleIAMAdministrator"
+
+  tmp_all_roles = [local.tf_provisioner_role, local.security_eng_role, local.changeset_creator_role, local.iam_admin_role]
 }
 
 module "state_bucket" {
@@ -26,6 +31,23 @@ module "state_bucket" {
 
   data_expiry      = "10-years"
   data_sensitivity = "high"
+
+  log_bucket_id = module.access_logs.bucket_id
+}
+
+module "cf_templates_bucket" {
+  source        = "hmrc/s3-bucket-standard/aws"
+  version       = "0.1.2"
+  bucket_name   = "cf-templates-1a94pgui3v5ft-eu-west-2"
+  force_destroy = false
+
+  list_roles          = local.tmp_all_roles
+  read_roles          = local.tmp_all_roles
+  write_roles         = local.tmp_all_roles
+  metadata_read_roles = local.tmp_all_roles
+
+  data_expiry      = "90-days"
+  data_sensitivity = "low"
 
   log_bucket_id = module.access_logs.bucket_id
 }
