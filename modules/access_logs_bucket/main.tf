c7f918445527f7f228cdc09808d77dfc1ddcf503
wunzeco <1915636+wunzeco@users.noreply.github.com>
2022-12-08 11:44:46 +0000

    PSEC-1724 refactor access_logs_bucket config and disable access logging

--- modules/access_logs_bucket/main.tf
+++ modules/access_logs_bucket/main.tf
@@ -98,6 +98,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "access_logs" {
     }
   }
 }
+
 resource "aws_s3_bucket_policy" "access_logs" {
   bucket     = aws_s3_bucket.access_logs.id
   policy     = data.aws_iam_policy_document.access_logs.json
