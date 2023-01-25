2dc0211eb314b41c0e3feb36f47bd61f50aab08d
cob16 <public+github@cormacbrady.info>
2023-01-25 13:33:04 +0000

    PSEC-1782 create lambda ecr repos in ci account

---
+++ ecr_repositories.tf
@@ -0,0 +1,34 @@
+module "bitwarden-manager-repository" {
+  source = "./modules/ecr_repository"
+
+  repository_name            = "bitwarden-manager"
+  allow_read_account_id_list = local.all_account_ids
+}
+
+module "aws-scanner-repository" {
+  source = "./modules/ecr_repository"
+
+  repository_name = "aws-scanner"
+  allow_read_account_id_list = local.all_account_ids
+}
+
+module "compliance-alerting-repository" {
+  source = "./modules/ecr_repository"
+
+  repository_name = "compliance-alerting"
+  allow_read_account_id_list = local.all_account_ids
+}
+
+module "github-admin-report-repository" {
+  source = "./modules/ecr_repository"
+
+  repository_name = "github-admin-report"
+  allow_read_account_id_list = local.all_account_ids
+}
+
+module "github-webhook-report-repository" {
+  source = "./modules/ecr_repository"
+
+  repository_name = "github-webhook-report"
+  allow_read_account_id_list = local.all_account_ids
+}
