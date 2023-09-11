457402f6614c75c0febca265a53bdb7db342303b
Reece Crossland <10209831+ReeceCrossland@users.noreply.github.com>
2023-09-11 13:28:47 +0100

    NOJIRA refactoring local to more suitable name

--- ecr_repositories.tf
+++ ecr_repositories.tf
@@ -2,21 +2,21 @@ module "bitwarden_manager_repository" {
   source = "./modules/ecr_repository"
 
   repository_name            = "bitwarden-manager"
-  allow_read_account_id_list = local.all_account_ids
+  allow_read_account_id_list = local.all_platsec_account_ids
 }
 
 module "aws_scanner_repository" {
   source = "./modules/ecr_repository"
 
   repository_name            = "aws-scanner"
-  allow_read_account_id_list = local.all_account_ids
+  allow_read_account_id_list = local.all_platsec_account_ids
 }
 
 module "cloudtrail_monitoring_repository" {
   source = "./modules/ecr_repository"
 
   repository_name            = "cloudtrail-monitoring"
-  allow_read_account_id_list = local.all_account_ids
+  allow_read_account_id_list = local.all_platsec_account_ids
 }
 
 
@@ -24,28 +24,28 @@ module "compliance_alerting_repository" {
   source = "./modules/ecr_repository"
 
   repository_name            = "compliance-alerting"
-  allow_read_account_id_list = local.all_account_ids
+  allow_read_account_id_list = local.all_platsec_account_ids
 }
 
 module "github_admin_report_repository" {
   source = "./modules/ecr_repository"
 
   repository_name            = "github-admin-report"
-  allow_read_account_id_list = local.all_account_ids
+  allow_read_account_id_list = local.all_platsec_account_ids
 }
 
 module "github_webhook_report_repository" {
   source = "./modules/ecr_repository"
 
   repository_name            = "github-webhook-report"
-  allow_read_account_id_list = local.all_account_ids
+  allow_read_account_id_list = local.all_platsec_account_ids
 }
 
 module "monitor_aws_iam_repository" {
   source = "./modules/ecr_repository"
 
   repository_name            = "monitor-aws-iam"
-  allow_read_account_id_list = local.all_account_ids
+  allow_read_account_id_list = local.all_platsec_account_ids
 }
 
 module "go_nuke_repository" {
@@ -59,5 +59,5 @@ module "security_reports_frontend_repository" {
   source = "./modules/ecr_repository"
 
   repository_name            = "security-reports-frontend"
-  allow_read_account_id_list = local.all_account_ids
+  allow_read_account_id_list = local.all_platsec_account_ids
 }
