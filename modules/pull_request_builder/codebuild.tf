557cedb378df0171d94136d05862ef2e0131ae1b
wunzeco <1915636+wunzeco@users.noreply.github.com>
2023-11-07 14:59:42 +0000

    PSEC-1985 update codebuild image

--- modules/pull_request_builder/codebuild.tf
+++ modules/pull_request_builder/codebuild.tf
@@ -22,7 +22,7 @@ resource "aws_codebuild_project" "build" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:6.0"
+    image                       = "aws/codebuild/standard:7.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = var.docker_required
