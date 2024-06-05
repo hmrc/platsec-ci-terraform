9709aeb4e9af1c57b7aa29c5a135b95a7ac1972c
wunzeco <1915636+wunzeco@users.noreply.github.com>
2024-06-05 11:58:31 +0100

    PSEC-2152 provision pr builder for platsec-catalogue

--- pull_request_builder.tf
+++ pull_request_builder.tf
@@ -21,6 +21,7 @@ module "platsec_terraform_pr_builder" {
     local.ci_agent_to_endpoints_sg_id
   ]
 }
+
 module "central_audit_pr_builder" {
 
   source = "./modules/pull_request_builder"
@@ -36,6 +37,26 @@ module "central_audit_pr_builder" {
     "DEVELOPMENT_TERRAFORM_PLANNER_ROLE_ARN" = local.accounts.central_audit_development.role_arns["terraform-planner"]
     "PRODUCTION_TERRAFORM_PLANNER_ROLE_ARN"  = local.accounts.central_audit_production.role_arns["terraform-planner"]
   }
+
+  vpc_config = local.vpc_config
+  agent_security_group_ids = [
+    local.ci_agent_to_internet_sg_id,
+    local.ci_agent_to_endpoints_sg_id
+  ]
+}
+
+module "platsec_catalogue_pr_builder" {
+
+  source = "./modules/pull_request_builder"
+
+  src_repo              = "platsec-catalogue"
+  buildspec             = "platsec-catalogue.yaml"
+  docker_required       = true
+  project_name          = "platsec-catalogue-pr-builder"
+  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
+
+  admin_role           = local.tf_admin_role
+  project_assume_roles = {}
 
   vpc_config = local.vpc_config
   agent_security_group_ids = [
