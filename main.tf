99f3e506b049ed6a77bda839ebe9f52a9b7a2326
wunzeco <1915636+wunzeco@users.noreply.github.com>
2024-08-07 10:00:15 +0100

    PSEC-2203 update aws provider version constraint

--- main.tf
+++ main.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-west-2"
-  default_tags {
-    tags = module.label.tags
-  }
-}
-
-# https://github.com/terraform-aws-modules/terraform-aws-vpc/issues/625
-provider "aws" {
-  alias  = "no-default-tags"
-  region = "eu-west-2"
-}
-
 locals {
   is_live                = terraform.workspace == "live"
   prefix                 = local.is_live ? "platsec-ci-" : "platsec-${terraform.workspace}-"
