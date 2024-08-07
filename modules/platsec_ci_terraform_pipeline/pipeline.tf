3f9d7ee0ca95ff4f8654d7b46a4e26a0f3235d39
wunzeco <1915636+wunzeco@users.noreply.github.com>
2024-08-07 09:31:20 +0100

    PSEC-2203 add new modules

---
+++ modules/platsec_ci_terraform_pipeline/pipeline.tf
@@ -0,0 +1,122 @@
+resource "aws_codepipeline" "codepipeline" {
+  name     = module.common.pipeline_name
+  role_arn = module.common.codepipeline_role_arn
+
+  artifact_store {
+    location = module.common.bucket_id
+    type     = "S3"
+
+    encryption_key {
+      id   = module.common.kms_key_arn
+      type = "KMS"
+    }
+  }
+
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      namespace        = "SourceVariables"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner                = var.src_org
+        Repo                 = var.src_repo
+        PollForSourceChanges = false
+        Branch               = var.branch
+        OAuthToken           = var.github_token
+      }
+    }
+  }
+
+  stage {
+    name = "pre-apply"
+
+    action {
+      name            = "create-timestamp"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
+      version         = "1"
+      namespace       = "Timestamp"
+
+      configuration = {
+        ProjectName = module.build_timestamp_step.name
+      }
+    }
+  }
+
+  stage {
+    name = "apply-ci"
+
+    action {
+      name            = "apply-ci"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = module.apply_step["ci"].name
+        EnvironmentVariables = jsonencode([
+          {
+            name  = "COMMIT_ID"
+            value = "#{SourceVariables.CommitId}"
+            type  = "PLAINTEXT"
+          }
+        ])
+      }
+    }
+  }
+
+  stage {
+    name = "approve-production"
+
+    action {
+      name     = "approve-production"
+      category = "Approval"
+      owner    = "AWS"
+      provider = "Manual"
+      version  = "1"
+
+      configuration = {
+        ExternalEntityLink : "https://github.com/${var.src_org}/${var.src_repo}/commit/#{SourceVariables.CommitId}"
+        CustomData : "#{SourceVariables.CommitMessage}"
+      }
+    }
+  }
+
+  stage {
+    name = "apply-main"
+
+    action {
+      name            = "apply-main"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = module.apply_step["main"].name
+        EnvironmentVariables = jsonencode([
+          {
+            name  = "COMMIT_ID"
+            value = "#{SourceVariables.CommitId}"
+            type  = "PLAINTEXT"
+          }
+        ])
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [stage[0].action[0].configuration["OAuthToken"]]
+  }
+}
