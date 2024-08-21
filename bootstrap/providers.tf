a906468a84455d7aa66a5c80c6942f2bd683e0f5
Glyn Mooney <749058+skidoosh@users.noreply.github.com>
2024-08-21 14:00:29 +0100

    PSEC-2210 Added default tags and brought kms inline wiith proposed policy

--- bootstrap/providers.tf
+++ bootstrap/providers.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      team        = "platsec"
+      region      = "eu-west-2"
+      environment = "bootstrap"
+    }
+  }
 }
