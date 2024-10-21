08f997915e7b4acc5ac019740550f0872b07ace6
Glyn Mooney <749058+skidoosh@users.noreply.github.com>
2024-10-21 10:50:38 +0100

    PSEC-2231 KMS policy

--- modules/renovatebot_pipeline/kms.tf
+++ modules/renovatebot_pipeline/kms.tf
@@ -1,5 +1,34 @@
-resource "aws_kms_key" "this" {
+data "aws_iam_policy_document" "kms" {
+  statement {
+    sid       = "RenovateAllowPermissionsIamUsers"
+    actions   = ["kms:*"]
+    resources = ["*"]
+    principals {
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+      type        = "AWS"
+    }
+  }
 
+  statement {
+    sid = "RenovateAllowPermissionsPipelineRoles"
+    actions = [
+      "kms:GenerateDataKey",
+      "kms:Decrypt"
+    ]
+    resources = ["*"]
+    principals {
+      identifiers = var.admin_roles
+      type        = "AWS"
+    }
+  }
+}
+
+resource "aws_kms_key" "this" {
+  bypass_policy_lockout_safety_check = false
+  deletion_window_in_days            = 7
+  enable_key_rotation                = true
+  rotation_period_in_days            = 90
+  policy                             = data.aws_iam_policy_document.kms.json
 }
 
 resource "aws_kms_alias" "this" {
