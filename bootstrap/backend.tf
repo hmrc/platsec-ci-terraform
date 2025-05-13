0d7f3c98c553686c469a96884ecd669d482a0fb1
rizinaa99 <66684341+rizinaa99@users.noreply.github.com>
2025-05-13 10:58:50 +0100

    PSEC-2512 remove DynamoDB locking and add use_lockfile to backend config

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -9,11 +9,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "platsec-ci20210713082841419000000002"
-    region         = "eu-west-2"
-    key            = "bootstrap/v1"
-    encrypt        = true
-    kms_key_id     = "alias/s3-platsec-ci20210713082841419000000002"
-    dynamodb_table = "platsec-ci"
+    bucket       = "platsec-ci20210713082841419000000002"
+    region       = "eu-west-2"
+    key          = "bootstrap/v1"
+    use_lockfile = true
+    encrypt      = true
+    kms_key_id   = "alias/s3-platsec-ci20210713082841419000000002"
   }
 }
