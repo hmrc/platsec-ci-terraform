89727bda4b370c13fcd3734ca4af02dec0386498
DannyLyubenov <23269399+DannyLyubenov@users.noreply.github.com>
2025-06-19 10:34:18 +0100

    PSEC-2566: Add ReadOnly ProwlerScanner Access

--- modules/pipeline_common/s3.tf
+++ modules/pipeline_common/s3.tf
@@ -78,4 +78,32 @@ data "aws_iam_policy_document" "bucket_policy" {
       values   = var.admin_roles
     }
   }
+
+  statement {
+    sid    = "AllowReadOnlyProwlerScanner"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:ListBucket*",
+      "s3:GetObject*",
+      "s3:GetBucket*",
+      "s3:GetEncryptionConfiguration",
+      "s3:GetInventoryConfiguration",
+      "s3:GetLifecycleConfiguration",
+      "s3:GetMetricsConfiguration",
+      "s3:GetReplicationConfiguration"
+    ]
+    resources = [
+      module.codepipeline_bucket.arn,
+      "${module.codepipeline_bucket.arn}/*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalArn"
+      values   = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RoleProwlerScanner"]
+    }
+  }
 }
