11fb521d9952132e5cdb1ff81900562b79cc9572
Glyn Mooney <749058+skidoosh@users.noreply.github.com>
2025-10-22 13:49:08 +0100

    PSEC-2824 Remove redundant buckets from legacy pr builder (#212)
    * PSEC-2824 Remove redundant buckets from legacy pr builder
    
    * PSEC-2824 Fix for the ci pr builder
    
    * PSEC-2824 Fix formatting

--- live/pull_request_builder.tf
+++ live/pull_request_builder.tf
@@ -2,14 +2,11 @@ module "platsec_catalogue_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "platsec-catalogue"
-  buildspec             = "platsec-catalogue.yaml"
-  docker_required       = true
-  project_name          = "platsec-catalogue-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "platsec-catalogue"
+  buildspec            = "platsec-catalogue.yaml"
+  docker_required      = true
+  project_name         = "platsec-catalogue-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -24,14 +21,11 @@ module "monitor_aws_iam_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "monitor-aws-iam"
-  buildspec             = "monitor-aws-iam.yaml"
-  docker_required       = true
-  project_name          = "monitor-aws-iam-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "monitor-aws-iam"
+  buildspec            = "monitor-aws-iam.yaml"
+  docker_required      = true
+  project_name         = "monitor-aws-iam-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -46,14 +40,11 @@ module "aws_scanner_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "platsec-aws-scanner"
-  buildspec             = "aws-scanner.yaml"
-  docker_required       = true
-  project_name          = "aws-scanner-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "platsec-aws-scanner"
+  buildspec            = "aws-scanner.yaml"
+  docker_required      = true
+  project_name         = "aws-scanner-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -68,14 +59,11 @@ module "bitwarden_manager_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "bitwarden-manager"
-  buildspec             = "bitwarden-manager.yaml"
-  docker_required       = true
-  project_name          = "bitwarden-manager-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "bitwarden-manager"
+  buildspec            = "bitwarden-manager.yaml"
+  docker_required      = true
+  project_name         = "bitwarden-manager-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -90,14 +78,11 @@ module "aws_users_manager_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "aws-users-manager"
-  buildspec             = "aws-users-manager.yaml"
-  docker_required       = true
-  project_name          = "aws-users-manager-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "aws-users-manager"
+  buildspec            = "aws-users-manager.yaml"
+  docker_required      = true
+  project_name         = "aws-users-manager-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -112,14 +97,11 @@ module "prowler_scanner_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "prowler-scanner"
-  buildspec             = "prowler-scanner.yaml"
-  docker_required       = true
-  project_name          = "prowler-scanner-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "prowler-scanner"
+  buildspec            = "prowler-scanner.yaml"
+  docker_required      = true
+  project_name         = "prowler-scanner-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -134,14 +116,11 @@ module "prowler_scan_enqueuer_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "prowler-scan-enqueuer"
-  buildspec             = "prowler-scan-enqueuer.yaml"
-  docker_required       = true
-  project_name          = "prowler-scan-enqueuer-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "prowler-scan-enqueuer"
+  buildspec            = "prowler-scan-enqueuer.yaml"
+  docker_required      = true
+  project_name         = "prowler-scan-enqueuer-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -156,14 +135,11 @@ module "cloudtrail_monitoring_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "platsec-cloudtrail-monitoring"
-  buildspec             = "cloudtrail-monitoring.yaml"
-  docker_required       = true
-  project_name          = "cloudtrail-monitoring-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "platsec-cloudtrail-monitoring"
+  buildspec            = "cloudtrail-monitoring.yaml"
+  docker_required      = true
+  project_name         = "cloudtrail-monitoring-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -178,14 +154,11 @@ module "compliance_alerting_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "platsec-compliance-alerting"
-  buildspec             = "compliance-alerting.yaml"
-  docker_required       = true
-  project_name          = "compliance-alerting-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "platsec-compliance-alerting"
+  buildspec            = "compliance-alerting.yaml"
+  docker_required      = true
+  project_name         = "compliance-alerting-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -200,14 +173,11 @@ module "compliance_dataviz_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "platsec-compliance-dataviz"
-  buildspec             = "compliance-dataviz.yaml"
-  docker_required       = true
-  project_name          = "compliance-dataviz-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "platsec-compliance-dataviz"
+  buildspec            = "compliance-dataviz.yaml"
+  docker_required      = true
+  project_name         = "compliance-dataviz-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -222,14 +192,11 @@ module "github_scanner_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "platsec-scanning-tools"
-  buildspec             = "github-scanner.yaml"
-  docker_required       = true
-  project_name          = "github-scanner-pr-builder"
-  access_logs_bucket_id = local.access_log_bucket_id
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "platsec-scanning-tools"
+  buildspec            = "github-scanner.yaml"
+  docker_required      = true
+  project_name         = "github-scanner-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -244,14 +211,11 @@ module "vault_policy_applier_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "vault-policy-applier"
-  buildspec             = "vault-policy-applier.yaml"
-  docker_required       = true
-  project_name          = "vault-policy-applier-pr-builder"
-  access_logs_bucket_id = local.access_log_bucket_id
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "vault-policy-applier"
+  buildspec            = "vault-policy-applier.yaml"
+  docker_required      = true
+  project_name         = "vault-policy-applier-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
@@ -266,15 +230,12 @@ module "policyexplorer_pr_builder" {
 
   source = "../modules//pull_request_builder"
 
-  codeconnection_arn    = data.aws_codestarconnections_connection.this.arn
-  src_repo              = "policyexplorer"
-  buildspec             = "policyexplorer.yaml"
-  docker_required       = true
-  badge_enabled         = true
-  project_name          = "policyexplorer-pr-builder"
-  access_logs_bucket_id = data.aws_secretsmanager_secret_version.s3_access_logs_bucket_name.secret_string
-
-  admin_roles          = local.tf_admin_roles
+  codeconnection_arn   = data.aws_codestarconnections_connection.this.arn
+  src_repo             = "policyexplorer"
+  buildspec            = "policyexplorer.yaml"
+  docker_required      = true
+  badge_enabled        = true
+  project_name         = "policyexplorer-pr-builder"
   project_assume_roles = {}
 
   vpc_config               = local.vpc_config
