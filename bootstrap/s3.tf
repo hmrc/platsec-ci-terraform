0c48b6f3372252763cb59d1d986bf1abcec48e02
masquerainsley <43313008+Masquerainsley@users.noreply.github.com>
2026-03-04 13:20:04 +0000

    PSEC-2739 - add public access block into bootstrap

--- bootstrap/s3.tf
+++ bootstrap/s3.tf
@@ -71,3 +71,12 @@ module "cf_templates_bucket" {
 
   log_bucket_id = module.access_logs.bucket_id
 }
+
+resource "aws_s3_account_public_access_block" "this" {
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+
